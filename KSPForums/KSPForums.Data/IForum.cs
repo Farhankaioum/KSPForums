442851cb84d2bb3595bd21f85e860a476ab574fb
FarhanKaioum <km1472233@gmail.com>
2020-02-24 15:07:24 +0600

    Service layed added

---
+++ KSPForums/KSPForums.Data/IForum.cs
@@ -0,0 +1,21 @@
+﻿using KSPForums.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSPForums.Data
+{
+    public interface IForum
+    {
+        public Forum GetById(int id);
+        public IEnumerable<Forum> GetAll();
+        public IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        public Task Create(Forum forum);
+        public Task Delete(int forumId);
+        public Task UpdateForumTitle(int forumId, string newTitle);
+        public Task UpdateForumDescription (int forumId, string newDescription);
+        
+    }
+}
